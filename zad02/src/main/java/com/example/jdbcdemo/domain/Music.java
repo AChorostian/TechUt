2d8fe293b87448c2907c132e5bd202b2569cfa57
mojave <mojave@172.20.10.3>
2018-11-07 16:47:41 +0100

    write start code

--- zad02/src/main/java/com/example/jdbcdemo/domain/Music.java
+++ zad02/src/main/java/com/example/jdbcdemo/domain/Music.java
@@ -1,37 +1,30 @@
 package com.example.jdbcdemo.domain;
 
-public class Person {
+public class Music {
 	
-	private long id;
+	private String autor;
+	private String title;
 	
-	private String name;
-	private int yob;
-	
-	public Person() {
-	}
-	
-	public Person(String name, int yob) {
-		super();
-		this.name = name;
-		this.yob = yob;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getYob() {
-		return yob;
-	}
-	public void setYob(int yob) {
-		this.yob = yob;
+	public Music( String autor , String title )
+	{
+		this.autor = autor;
+		this.title = title;
 	}
 	
+	public String getAutor() 
+	{
+		return this.autor;
+	}
+	public String getTitle() 
+	{
+		return this.title;
+	}
+	public void setAutor(String autor) 
+	{
+		this.autor = autor;
+	}
+	public void setTitle(String title) 
+	{
+		this.title = title;
+	}
 }
